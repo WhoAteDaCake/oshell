b13ac70082011c57f561c82f4c5738a17f42575b
WhoAteDaCake <doitsimplefy@gmail.com>
2018-11-30 12:02:04 +0000

    add basic shell loop

--- lib/main.ml
+++ lib/main.ml
@@ -1,8 +1,17 @@
-let cmd_prefix path = path ^ " $ "
+let pretify path =
+  let parts = String.split_on_char '/' path in
+  let partial = match List.rev parts with
+  | x :: [] -> "/" ^ x
+  | x :: y :: xs -> y ^ "/" ^ x
+  (* Shouldn't really ever hapen with linux paths *)
+  | _ -> "unknown"
+  in
+  Printf.sprintf "[%s] $" partial
+
 
 let cmd_line path out = 
   let open Lwt.Infix in
-  Lwt_io.fprint out (cmd_prefix path)
+  Lwt_io.fprint out (pretify path)
   >>= (fun _ -> Lwt_io.flush out)
 
 let rec main path = 
