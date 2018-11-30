2b5bfaf97c46549ea18612ac0b5e6854597626bf
WhoAteDaCake <doitsimplefy@gmail.com>
2018-11-30 13:41:38 +0000

    add ls command

---
+++ lib/commands/ls.ml
@@ -0,0 +1,18 @@
+let not_special_path p = p <> "." && p <> ".."
+
+let dir_entries path =
+  let rec loop desc xs =
+    match Unix.readdir desc with
+    | exception End_of_file -> xs
+    | entry -> loop desc (entry :: xs)
+  in
+  let handle = Unix.opendir path in
+  let entries = loop handle [] in
+  Unix.closedir handle;
+  List.filter not_special_path entries
+
+let run out path args = 
+  let open Lwt.Infix in
+  let entries = dir_entries path in
+  Lwt_list.map_s (fun p -> Lwt_io.fprint out (p ^ " ")) entries
+  >>= (fun _ -> Lwt_io.fprint out "\n")
--- lib/main.ml
+++ lib/main.ml
@@ -1,12 +1,12 @@
 let pretify path =
   let parts = String.split_on_char '/' path in
   let partial = match List.rev parts with
-  | x :: [] -> "/" ^ x
-  | x :: y :: xs -> y ^ "/" ^ x
-  (* Shouldn't really ever hapen with linux paths *)
-  | _ -> "unknown"
-  in
-  Printf.sprintf "[%s] $ " partial
+    | x :: [] -> "/" ^ x
+    | x :: y :: xs -> y ^ "/" ^ x
+    (* Shouldn't really ever hapen with linux paths *)
+    | _ -> "unknown"
+    in
+    Printf.sprintf "[%s] $ " partial
 
 
 let cmd_line path out = 
@@ -14,8 +14,21 @@ let cmd_line path out =
   Lwt_io.fprint out (pretify path)
   >>= (fun _ -> Lwt_io.flush out)
 
+let no_runner out path args = Lwt.return ()
+
+let handle command =
+  match String.split_on_char ' ' command with
+  | [] ->  (no_runner, [])
+  | cmd :: args ->
+    match cmd with
+    | "ls" -> (Ls.run, args) 
+    | _ -> (no_runner, args)
+
 let rec main path = 
   let open Lwt.Infix in
-  cmd_line path Lwt_io.stdout
-  >>= (fun c -> Lwt_io.read_line Lwt_io.stdin)
-  >>= (fun c -> main path)
+  let out = Lwt_io.stdout in
+  cmd_line path out
+  >>= (fun _ -> Lwt_io.read_line Lwt_io.stdin)
+  >|= handle
+  >>= (fun (runner, args) -> runner out path args)
+  >>= (fun _ -> main path)
