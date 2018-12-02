f9621cb8c7f95cfcbc05cacb5d2dc5c9726d5bee
WhoAteDaCake <doitsimplefy@gmail.com>
2018-12-02 10:43:27 +0000

    update path utils

--- lib/utils/path.ml
+++ lib/utils/path.ml
@@ -1,4 +1,5 @@
 let append p path =
   let open Fpath in
-  Result_fn.lift2 (fun p path -> append p path) (of_string p) (of_string path)
-  |> to_string
+  let open Result_fn in
+  lift2 (fun p path -> append p path) (of_string p) (of_string path)
+  >|= to_string
